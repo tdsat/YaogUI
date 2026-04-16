f961a99ec011805689571f91fcfea89b2a9266e8
Spartakos <tdspartakos@gmail.com>
2026-04-16 18:50:34 +0300

    (WIP) Clear all items

--- Source/YaogUI/StorageArea.cs
+++ Source/YaogUI/StorageArea.cs
@@ -7,6 +7,7 @@ using XiaWorld;
 using XiaWorld.ThingStep;
 using System.Linq;
 using KTV;
+using UnityEngine;
 
 namespace YaogUI
 {
@@ -24,11 +25,11 @@ namespace YaogUI
 				var clearAll = UIPackage.CreateObjectFromURL("ui://ncbwb41mv9j6ah");
 				clearAll.name = "YaogUI.ClearAll";
 				clearAll.visible = true;
-				clearAll.text = TFMgr.Get("明确");
+				clearAll.text = TFMgr.Get("全部清除");
 
 				// Place buttons
-				clearAll.x = UI.width - 10;
-				clearAll.y = 10;
+				clearAll.x = UI.width - 50;
+				clearAll.y = 50;
 
 				var clearItems = UIPackage.CreateObjectFromURL("ui://ncbwb41mv9j6ah");
 				clearAll.name = "YaogUI.ClearItems";
@@ -39,10 +40,15 @@ namespace YaogUI
 				clearItems.y = clearAll.y + 30;
 
 				// Add callbacks
-				clearAll.onClick.Add((e) =>
+				clearAll.onClick.Add(delegate(EventContext context)
 				{
 					Main.Debug("All");
-					area.ExcludeItemLables = new List<g_emItemLable>();
+					for (g_emItemKind groupIndex = g_emItemKind.None; groupIndex < g_emItemKind.Count; ++groupIndex)
+					{
+						UI_Item_Storage group = UI.m_n25.GetChildAt((int) groupIndex) as UI_Item_Storage;
+						group.m_title.selected = false;
+						group.m_title.onClick.Call();
+					}
 				});
 
 				clearItems.onClick.Add((e) =>
@@ -53,18 +59,18 @@ namespace YaogUI
 				UI.AddChild(clearAll);
 				UI.AddChild(clearItems);
 				
-				for (g_emItemKind itemKind = g_emItemKind.None; itemKind < g_emItemKind.Count; itemKind++)
-				{
-					UI_Item_Storage group = UI.m_n25.GetChildAt((int) itemKind) as UI_Item_Storage;
-					group.m_title.onClick.Add(delegate (EventContext context)
-					{
-						Main.Debug("Clicked button");
-						if (context.inputEvent.ctrl)
-						{
-							Main.Debug("Control was pressed");
-						}
-					});
-				}
+				// for (g_emItemKind itemKind = g_emItemKind.None; itemKind < g_emItemKind.Count; itemKind++)
+				// {
+				// 	UI_Item_Storage group = UI.m_n25.GetChildAt((int) itemKind) as UI_Item_Storage;
+				// 	group.m_title.onClick.Add(delegate (EventContext context)
+				// 	{
+				// 		Main.Debug("Clicked button");
+				// 		if (context.inputEvent.ctrl)
+				// 		{
+				// 			Main.Debug("Control was pressed");
+				// 		}
+				// 	});
+				// }
 			}
 			catch (Exception e)
 			{
