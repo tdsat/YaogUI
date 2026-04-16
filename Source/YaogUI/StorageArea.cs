4face1c5b3dd714fe1ee3030a6f2f74414d7a3af
Spartakos <tdspartakos@gmail.com>
2026-04-16 17:46:40 +0300

    (WIP) Storage area window modifications

---
+++ Source/YaogUI/StorageArea.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using FairyGUI;
+using HarmonyLib;
+using XiaWorld.UI.InGame;
+using System.Collections.Generic;
+using XiaWorld;
+using XiaWorld.ThingStep;
+using System.Linq;
+using KTV;
+
+namespace YaogUI
+{
+	[HarmonyPatch(typeof(Wnd_StorageArea), "OnInit")]
+	public static class TestShit
+	{
+		public static void Postfix(Wnd_StorageArea __instance)
+		{
+			try
+			{
+				var UI = __instance.UIInfo;
+				var area = Traverse.Create(__instance).Field("area").GetValue<AreaStorage>();
+				
+				//UIPackage.CreateObjectFromURL("ui://ncbwb41mv9j6ah");
+				var clearAll = UIPackage.CreateObjectFromURL("ui://ncbwb41mv9j6ah");
+				clearAll.name = "YaogUI.ClearAll";
+				clearAll.visible = true;
+				clearAll.text = TFMgr.Get("明确");
+
+				// Place buttons
+				clearAll.x = UI.width - 10;
+				clearAll.y = 10;
+
+				var clearItems = UIPackage.CreateObjectFromURL("ui://ncbwb41mv9j6ah");
+				clearAll.name = "YaogUI.ClearItems";
+				clearAll.visible = true;
+				clearAll.text = TFMgr.Get("明确");
+
+				clearItems.x = clearAll.x;
+				clearItems.y = clearAll.y + 30;
+
+				// Add callbacks
+				clearAll.onClick.Add((e) =>
+				{
+					Main.Debug("All");
+					area.ExcludeItemLables = new List<g_emItemLable>();
+				});
+
+				clearItems.onClick.Add((e) =>
+				{
+					Main.Debug("Items");
+				});
+
+				UI.AddChild(clearAll);
+				UI.AddChild(clearItems);
+				
+				for (g_emItemKind itemKind = g_emItemKind.None; itemKind < g_emItemKind.Count; itemKind++)
+				{
+					UI_Item_Storage group = UI.m_n25.GetChildAt((int) itemKind) as UI_Item_Storage;
+					group.m_title.onClick.Add(delegate (EventContext context)
+					{
+						Main.Debug("Clicked button");
+						if (context.inputEvent.ctrl)
+						{
+							Main.Debug("Control was pressed");
+						}
+					});
+				}
+			}
+			catch (Exception e)
+			{
+				Main.Debug(e.ToString());
+			}
+		}
+	}
+}
